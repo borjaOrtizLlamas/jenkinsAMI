e40f98c51fea2ee3f30c55c97f1a9dcd23a6fdab
borja <borja.ortizllamas1@gmail.com>
2020-05-22 02:41:49 +0200

    config of jenkins

--- scrip.sh
+++ scrip.sh
@@ -2,3 +2,6 @@ sudo apt-get update
 sudo add-apt-repository universe -y
 sudo apt-get update
 sudo apt-get install -y ansible 
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo newgrp docker
